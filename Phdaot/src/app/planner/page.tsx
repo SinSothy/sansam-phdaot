0eb3762379b3adf392d123544d87bfbe26d0e6ec
SOTHY\SinSothy <sinsothy42@gmail.com>
2026-04-01 21:22:38 +0700

    05-CMCB#time 1h#comment remove unuse component after integrate with locale

--- Phdaot/src/app/planner/page.tsx
+++
@@ -1,9 +0,0 @@
-import { KanbanBoard } from "@/components/workspaces/KanbanBoard";
-
-export default function PlannerPage() {
-  return (
-    <div className="w-[calc(100%+4rem)] h-[calc(100vh-64px)] -m-8 bg-surface">
-      <KanbanBoard />
-    </div>
-  );
-}
--- Phdaot/src/app/tasks/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TaskList } from "@/components/TaskList";
-
-export default function TasksPage() {
-  return (
-    <div className="flex flex-col h-full w-full">
-      <TaskList />
-    </div>
-  );
-}
